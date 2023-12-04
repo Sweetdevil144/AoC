9beb328975990c2e17b7fb033ef9d04ea9afa5bc
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-12-04 14:15:50 +0530

    Day-4/30

--- Maths/consecutiveSumRiddle.go
+++ Maths/consecutiveSumRiddle.go
@@ -3,13 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		n int
-		x int
-	)
-	fmt.Scan(&n)
-	for n > 0 {
-		fmt.Scan(&x)
-
+	var testCases, number int
+	fmt.Scan(&testCases)
+	for testCases > 0 {
+		fmt.Scan(&number)
+		l, r := calculateResult(number)
+		fmt.Println(l, r)
+		testCases--
 	}
+}
+
+func calculateResult(num int) (int, int) {
+	if num == 1 {
+		return 0, 1
+	} else if num == 2 {
+		return -1, 2
+	}
+
+	factors := getFactors(num)
+	for _, factor := range factors {
+		otherFactor := num / factor
+		lowerBound := (2*factor - otherFactor + 1) / 2
+		upperBound := (2*factor + otherFactor - 1) / 2
+
+		if lowerBound < upperBound && (lowerBound+upperBound)*(upperBound-lowerBound+1)/2 == num {
+			return lowerBound, upperBound
+		}
+	}
+	return -1, -1
+}
+
+func getFactors(num int) []int {
+	var factors []int
+	for i := 1; i*i <= num; i++ {
+		if num%i == 0 {
+			factors = append(factors, i)
+			if otherFactor := num / i; otherFactor != i {
+				factors = append(factors, otherFactor)
+			}
+		}
+	}
+	return factors
 }
