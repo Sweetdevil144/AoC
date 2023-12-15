91e202b0348a007f54b28b13100692d92bc70a91
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-12-15 16:09:11 +0530

    Life's Fucked

--- TwoPointers/isPalindrome.go
+++ TwoPointers/isPalindrome.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unicode"
+)
 
+func main() {
+	fmt.Println(isPalindrome("race a car"))
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+}
+
+func isPalindrome(s string) bool {
+	var str = ""
+	for _, char := range s {
+		cond := (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
+		if cond {
+			str += string(unicode.ToLower(char))
+		}
+	}
+	var (
+		start = 0
+		end   = len(str) - 1
+	)
+	for start <= end {
+		if str[start] != str[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	fmt.Println(str)
+	return true
 }
